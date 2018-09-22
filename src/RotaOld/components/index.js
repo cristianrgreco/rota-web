cbabea157e4ec1b12eb880b64b2c88ade1929e85
Cristian Greco <cristian.greco@triptease.com>
2018-09-22 19:04:33 +0100

    Remove old rota module

--- src/RotaOld/components/index.js
+++
@@ -1,6 +0,0 @@
-export * from "./RotaRow";
-export * from "./RotaCell";
-export * from "./RotaEntry";
-export * from "./RotaHeader";
-export { default as PeriodSelector } from "./PeriodSelector";
-export { default as RotaEmployeePeriod } from "./RotaEmployeePeriod";
